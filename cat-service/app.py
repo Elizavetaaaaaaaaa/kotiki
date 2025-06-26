276a0602c466669cfb0ba4cfc9d47529173e66b8
Elizavetaaaaaaaaa <elizavetalavrentevaaa@mail.ru>
2025-06-27 01:02:17 +0300

    Update app.py

--- cat-service/app.py
+++ cat-service/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, send_from_directory
 from flask_socketio import SocketIO, emit
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
