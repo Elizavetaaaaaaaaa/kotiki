ae4afaa762c36ff1a786df49da3d6cd8e7038803
Elizavetaaaaaaaaa <elizavetalavrentevaaa@mail.ru>
2025-06-27 01:04:31 +0300

    Update app.py

--- cat-service/app.py
+++ cat-service/app.py
@@ -90,4 +90,4 @@ def handle_new_comment(data):
 
 if __name__ == '__main__':
     # Запускаем сервер SocketIO на всех интерфейсах, порт 5000
-    socketio.run(app, host='0.0.0.0', port=5000)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
